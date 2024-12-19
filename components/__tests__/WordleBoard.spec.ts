f98aa1955bd74a0e43023328d1eae8dd5c778aa9
Songklod Saengvoratip <songklod@marvelous.studio>
2024-12-19 11:15:43 +0700

    rename component name in test

--- components/__tests__/WordleBoard.spec.ts
+++ components/__tests__/WordleBoard.spec.ts
@@ -1,11 +1,11 @@
 // @vitest-environment nuxt
 import { describe, it, expect } from "vitest";
-import HelloWorld from "~/components/WordleBoard.vue";
+import WordleBoard from "~/components/WordleBoard.vue";
 import { mountSuspended } from "@nuxt/test-utils/runtime";
 
-describe("HelloWorld", async () => {
+describe("WordleBoard", async () => {
   it("render peoperly", async () => {
-    const wrapper = await mountSuspended(HelloWorld);
+    const wrapper = await mountSuspended(WordleBoard);
     expect(wrapper.html()).toContain("Hello, World!");
   });
 });
