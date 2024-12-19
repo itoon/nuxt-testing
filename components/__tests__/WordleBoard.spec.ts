deafc8c688b6bb1e29786a0982083c42aac3a094
Songklod Saengvoratip <songklod@marvelous.studio>
2024-12-19 11:32:25 +0700

    first red ให้ดูวิธีการตั้ง test name and comment ต่างๆ

--- components/__tests__/WordleBoard.spec.ts
+++ components/__tests__/WordleBoard.spec.ts
@@ -1,11 +1,24 @@
-// @vitest-environment nuxt
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, test } from "vitest";
 import WordleBoard from "~/components/WordleBoard.vue";
 import { mountSuspended } from "@nuxt/test-utils/runtime";
 
 describe("WordleBoard", async () => {
-  it("render peoperly", async () => {
-    const wrapper = await mountSuspended(WordleBoard);
-    expect(wrapper.html()).toContain("Hello, World!");
+  // it("render peoperly", async () => {
+  //   const wrapper = await mountSuspended(WordleBoard, {
+  //     props: { word: "World" },
+  //   });
+  //   expect(wrapper.html()).toContain("Hello, World!");
+  // });
+  test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
+    // Arrange
+    const wrapper = await mountSuspended(WordleBoard, {
+      props: { wordOfTheDay: "World" },
+    });
+    // Act
+    const guessInput = wrapper.find("input[type=text]");
+    await guessInput.setValue("World");
+    await guessInput.trigger("keydown.enter");
+    // Assert
+    expect(wrapper.html()).toContain("You win");
   });
 });
